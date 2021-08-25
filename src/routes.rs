a93d6b0d00448c2a3e498ebf9b78e33ce01ca74d
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-08-25 18:58:28 +0100

    fix errors

--- src/routes.rs
+++ src/routes.rs
@@ -253,7 +253,7 @@ pub async fn add_document(
 
     match payload.0 {
         DocumentOptions::Single(doc) => {
-            debug!("adding single document to index: {}", &index_name);
+            debug!("adding single document to index: {}", index_name.as_str());
             if wait {
                 check_error!(index.add_document(doc).await, "add document");
             } else {
@@ -265,7 +265,7 @@ pub async fn add_document(
             }
         }
         DocumentOptions::Many(docs) => {
-            debug!("adding multiple document to index: {}", &index_name);
+            debug!("adding multiple document to index: {}", index_name.as_str());
             if wait {
                 check_error!(index.add_many_documents(docs).await, "add documents");
             } else {
