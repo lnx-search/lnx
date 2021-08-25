df4be98e788211d0bf565f90888b159a4f7640f3
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-08-25 22:36:00 +0100

    add stop word loading

--- engine/src/stop_words.rs
+++ engine/src/stop_words.rs
@@ -1,25 +1,25 @@
 use std::io::Write;
 use anyhow::{Result, Error};
 use once_cell::sync::OnceCell;
-use flate2::read::GzDecoder;
+use flate2::write::GzDecoder;
 
 static STOP_WORDS: OnceCell<Vec<String>> = OnceCell::new();
 
 pub(crate) fn init_stop_words() -> Result<()> {
-    let data: &[u8] = include_bytes!("../_dist/stop_words");
+    let buffer: &[u8] = include_bytes!("../_dist/stop_words");
     let mut data = GzDecoder::new(vec![]);
     data.write_all(buffer)?;
     let data = data.finish()?;
 
     let words = String::from_utf8(data)
-        .map_err(|e|  Error::msg("failed to parse stop words from linked data."))?;
+        .map_err(|_|  Error::msg("failed to parse stop words from linked data."))?;
 
-    let mut words = vec![];
-    for word in words.split("\n") {
-        words.push(word.to_string())
+    let mut data = vec![];
+    for word in words.to_lowercase().split("\n") {
+        data.push(word.to_string())
     }
 
-    let _ = STOP_WORDS.set(words);
+    let _ = STOP_WORDS.set(data);
 
     Ok(())
 }
