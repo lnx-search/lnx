e53a6175133b2cfc242c863d0562718a0240035e
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-08-30 10:34:24 +0100

    add a custom pool wrapper to allow for returning on drop

---
+++ engine/src/index/executor.rs
@@ -0,0 +1,83 @@
+use anyhow::{Result, Error};
+use std::sync::Arc;
+use std::borrow::Borrow;
+use crossbeam::queue::ArrayQueue;
+use tantivy::Executor;
+
+#[derive(Clone)]
+pub(super) struct ExecutorPool {
+    executors: Arc<ArrayQueue<Executor>>,
+}
+
+impl ExecutorPool {
+    pub(super) fn create(index_name: &str, pool_size: usize, threads_per_reader: usize) -> Result<Self> {
+        let executors = ArrayQueue::new(pool_size);
+        for i in 0..pool_size {
+            let executor = if threads_per_reader > 1 {
+                info!(
+                    "[ READER {} @ {} ] executor startup, mode: multi-threaded, threads: {}",
+                    i, index_name, threads_per_reader
+                );
+                Executor::multi_thread(threads_per_reader, "index-reader-")?
+            } else {
+                info!(
+                    "[ READER {} @ {} ] executor startup, mode: single-threaded (no-op)",
+                    i, index_name,
+                );
+                Executor::single_thread()
+            };
+
+            if let Err(_) = executors.push(executor) {
+                panic!("executor pool was full yet executor was not added on creation, this is a bug.")
+            };
+        }
+
+        let executors = Arc::new(executors);
+
+        Ok(Self {
+            executors,
+        })
+    }
+
+    pub(super) fn shutdown(&self) {
+        while let Some(exec) = self.executors.pop() {
+            drop(exec);
+        }
+    }
+
+    pub(super) fn acquire(&self) -> Result<ExecutorHandle> {
+        debug!("taking executor from pool");
+        if let Some(executor) = self.executors.pop() {
+            Ok(ExecutorHandle {
+                inner: Some(executor),
+                queue: self.executors.clone(),
+            })
+        } else {
+            Err(Error::msg("no executors were available even though the max concurrent was not reached"))
+        }
+    }
+}
+
+pub(super) struct ExecutorHandle {
+    inner: Option<Executor>,
+    queue: Arc<ArrayQueue<Executor>>,
+}
+
+impl Borrow<Executor> for ExecutorHandle {
+    fn borrow(&self) -> &Executor {
+        let ref_ = self.inner.as_ref();
+        ref_.expect("extract executor")
+    }
+}
+
+impl Drop for ExecutorHandle {
+    fn drop(&mut self) {
+        debug!("returning executor to pool");
+        if let Some(inner) = self.inner.take() {
+            let maybe_err = self.queue.push(inner);
+            if maybe_err.is_err() {
+                panic!("failed to return executor to pool");
+            }
+        };
+    }
+}
