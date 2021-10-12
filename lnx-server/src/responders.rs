689255ba37b70fab9aaf7970aac38ce8128985a9
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-10-12 21:20:15 +0100

    fix status code issue

--- lnx-server/src/responders.rs
+++ lnx-server/src/responders.rs
@@ -1,6 +1,7 @@
 use headers::{ContentType, HeaderMapExt};
 use hyper::{Body, StatusCode};
 use serde::Serialize;
+use hyper::http::StatusCode;
 
 use crate::error::Result;
 
@@ -18,7 +19,7 @@ pub fn json_response<T: Serialize + ?Sized>(
 
     let buffer = serde_json::to_vec(&payload)?;
     let mut resp = hyper::Response::new(Body::from(buffer));
-    *resp.status_mut() = StatusCode::OK;
+    *resp.status_mut() = StatusCode::from_u16(status)?;
     resp.headers_mut().typed_insert(ContentType::json());
 
     Ok(resp)
