2949bffa021cc74cf0b1fb1f2388e97b7062b0a4
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2022-02-11 20:25:21 +0000

    add tokenizer

--- lnx-common/src/lib.rs
+++ lnx-common/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod tokenizer;
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ lnx-common/src/tokenizer/mod.rs
@@ -0,0 +1,73 @@
+use deunicode::deunicode_char;
+use tantivy::tokenizer::{
+    BoxTokenStream,
+    SimpleTokenizer,
+    Token,
+    TokenStream,
+    Tokenizer,
+};
+
+#[derive(Clone)]
+pub struct SimpleUnicodeTokenizer;
+
+impl Default for SimpleUnicodeTokenizer {
+    fn default() -> Self {
+        Self
+    }
+}
+
+pub fn produce_tokens(text: &str) -> Vec<Token> {
+    let mut characters = String::with_capacity(text.len());
+    for char in text.chars() {
+        if let Some(ascii) = deunicode_char(char) {
+            if ascii.len() > 1 {
+                characters.push(' ');
+            }
+            characters.push_str(&ascii.to_lowercase());
+        }
+    }
+
+    let simple = SimpleTokenizer {};
+    let mut stream = simple.token_stream(&characters);
+
+    let mut tokens = vec![];
+    while let Some(token) = stream.next() {
+        tokens.push(token.clone());
+    }
+
+    tokens
+}
+
+pub struct SimpleTokenStream {
+    tokens: Vec<Token>,
+    pointer: usize,
+}
+
+impl Tokenizer for SimpleUnicodeTokenizer {
+    fn token_stream<'a>(&self, text: &'a str) -> BoxTokenStream<'a> {
+        let tokens = produce_tokens(text);
+
+        BoxTokenStream::from(SimpleTokenStream { tokens, pointer: 0 })
+    }
+}
+
+impl TokenStream for SimpleTokenStream {
+    fn advance(&mut self) -> bool {
+        if self.pointer < self.tokens.len() {
+            self.pointer += 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn token(&self) -> &Token {
+        // safe because our pointer cannot go beyond bounds
+        unsafe { self.tokens.get_unchecked(self.pointer - 1) }
+    }
+
+    fn token_mut(&mut self) -> &mut Token {
+        // safe because our pointer cannot go beyond bounds
+        unsafe { self.tokens.get_unchecked_mut(self.pointer - 1) }
+    }
+}
