8ee47870e072f07cff740cc5da805c3aa264425a
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2022-02-12 16:11:13 +0000

    add missing exports

--- lnx-common/src/schema/mod.rs
+++ lnx-common/src/schema/mod.rs
@@ -10,3 +10,5 @@ pub use schema::Schema;
 pub use options::{BaseOptions, BytesOptions, CalculatedIntOptions};
 pub use field_name::FieldName;
 pub use field_info::FieldInfo;
+pub use boost::BoostFactor;
+pub use error::SchemaError;
