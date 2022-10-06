0626b5dc1ea573571ec188df83694a0b103727a5
ChillFish8 <hburt2003@gmail.com>
2022-10-06 16:42:10 +0100

    Add blocking combiner

--- lnx-segments/src/blocking/combiner.rs
+++ lnx-segments/src/blocking/combiner.rs
@@ -1,16 +1,29 @@
-use std::io;
+use std::env::temp_dir;
 use std::io::{ErrorKind, SeekFrom};
-use std::path::Path;
-use datacake_crdt::HLCTimestamp;
-use tokio::fs::{self, File};
-use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt, BufWriter};
+use std::ops::Range;
+use std::path::{Path, PathBuf};
+use std::{cmp, io};
 
-use crate::{get_metadata_offsets, Metadata, METADATA_HEADER_SIZE};
-use crate::blocking::BlockingWriter;
+use datacake_crdt::HLCTimestamp;
+use tokio::fs;
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt};
+use uuid::Uuid;
+
 use crate::blocking::utils::read_metadata;
+use crate::blocking::BlockingWriter;
+use crate::meta_merger::{ManagedMeta, MetaFile};
+use crate::{MANAGED_FILE, METADATA_HEADER_SIZE, META_FILE, SPECIAL_FILES};
+
+const BUFFER_SIZE: usize = 64 << 10;
 
 pub struct BlockingCombiner {
     writer: BlockingWriter,
+    deletes_tmp_file: File,
+    deletes_tmp_path: PathBuf,
+
+    meta_file: MetaFile,
+    managed_file: ManagedMeta,
 }
 
 impl BlockingCombiner {
@@ -22,7 +35,21 @@ impl BlockingCombiner {
     ) -> io::Result<Self> {
         let writer = BlockingWriter::create(path, 0, index, segment_id).await?;
 
-        Ok(Self { writer })
+        let deletes_tmp_path = temp_dir().join(Uuid::new_v4().to_string());
+        let deletes_tmp_file = fs::OpenOptions::new()
+            .create(true)
+            .write(true)
+            .read(true)
+            .open(&deletes_tmp_path)
+            .await?;
+
+        Ok(Self {
+            writer,
+            deletes_tmp_file,
+            deletes_tmp_path,
+            meta_file: MetaFile::default(),
+            managed_file: ManagedMeta::default(),
+        })
     }
 
     /// Combines a segment into the new segment.
@@ -34,10 +61,12 @@ impl BlockingCombiner {
         let metadata = read_metadata(&mut segment).await?;
 
         // Set the seek position back to the end of the metadata.
-        segment.seek(SeekFrom::Start(METADATA_HEADER_SIZE as u64)).await?;
+        segment
+            .seek(SeekFrom::Start(METADATA_HEADER_SIZE as u64))
+            .await?;
 
-
-        let mut files = metadata.files()
+        let mut files = metadata
+            .files()
             .iter()
             .map(|v| (v.1.clone(), v.0.clone()))
             .collect::<Vec<_>>();
@@ -52,16 +81,12 @@ impl BlockingCombiner {
                 segment.seek(SeekFrom::Start(range.start)).await?;
             }
 
-            let mut buffer = [0; 64 << 10];
-            loop {
-                let n = segment.read(&mut buffer[..]).await?;
-
-                if n == 0 {
-                    break;
-                }
-
-                self.writer.write_all(&buffer[..n]).await?;
+            if SPECIAL_FILES.contains(&path.as_str()) {
+                self.merge_special_files(&mut segment, range, &path).await?;
+                continue;
             }
+
+            self.write_file(&mut segment, range).await?;
 
             let end = self.writer.current_pos();
             self.writer.add_file(&path, start..end);
@@ -71,11 +96,138 @@ impl BlockingCombiner {
     }
 
     /// Finalises any remaining buffers so that file is safely persisted to disk.
-    pub async fn finalise(self) -> io::Result<File> {
+    pub async fn finalise(mut self) -> io::Result<File> {
+        let meta = self
+            .meta_file
+            .to_json()
+            .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+
+        let managed = self
+            .managed_file
+            .to_json()
+            .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+
+        let meta_start = self.writer.current_pos();
+        self.writer.write_all(&meta).await?;
+        let meta_range = meta_start..self.writer.current_pos();
+
+        let managed_start = self.writer.current_pos();
+        self.writer.write_all(&managed).await?;
+        let managed_range = managed_start..self.writer.current_pos();
+
+        self.writer.add_file("meta.json", meta_range);
+        self.writer.add_file(".managed.json", managed_range);
+
+        self.deletes_tmp_file.seek(SeekFrom::Start(0)).await?;
+        copy_data(&mut self.writer.inner, &mut self.deletes_tmp_file).await?;
+
         self.writer.finalise().await
     }
 
     pub async fn abort(self) -> io::Result<()> {
+        drop(self.deletes_tmp_file);
+
+        let _ = fs::remove_file(&self.deletes_tmp_path).await;
         self.writer.abort().await
     }
+
+    async fn write_file(
+        &mut self,
+        reader: &mut File,
+        range: Range<u64>,
+    ) -> io::Result<()> {
+        let len = range.end - range.start;
+        let mut bytes_written = 0;
+        let mut buffer = [0; BUFFER_SIZE];
+        while bytes_written < len {
+            let n = reader.read(&mut buffer[..]).await?;
+
+            if n == 0 {
+                break;
+            }
+
+            let n = cmp::min(len - bytes_written, n as u64) as usize;
+            self.writer.write_all(&buffer[..n]).await?;
+
+            bytes_written += n as u64;
+        }
+
+        // Reset the cursor incase we read over the number of bytes we wanted.
+        reader.seek(SeekFrom::Start(range.end)).await?;
+
+        Ok(())
+    }
+
+    async fn merge_special_files(
+        &mut self,
+        reader: &mut File,
+        range: Range<u64>,
+        path: &str,
+    ) -> io::Result<()> {
+        match path {
+            p if p == META_FILE => {
+                let data = read_range(reader, range.clone()).await?;
+
+                let meta = MetaFile::from_json(&data)
+                    .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+
+                self.meta_file.merge(meta);
+            },
+            p if p == MANAGED_FILE => {
+                let data = read_range(reader, range.clone()).await?;
+
+                let meta = ManagedMeta::from_json(&data)
+                    .map_err(|e| io::Error::new(ErrorKind::Other, e))?;
+
+                self.managed_file.merge(meta);
+            },
+            _ => return Ok(()),
+        };
+
+        // Reset the cursor incase we read over the number of bytes we wanted.
+        reader.seek(SeekFrom::Start(range.end)).await?;
+
+        Ok(())
+    }
+}
+
+async fn read_range(reader: &mut File, range: Range<u64>) -> io::Result<Vec<u8>> {
+    let mut data = vec![];
+
+    let len = range.end - range.start;
+    let mut bytes_written = 0;
+    let mut buffer = [0; BUFFER_SIZE];
+    while bytes_written < len {
+        let n = reader.read(&mut buffer[..]).await?;
+
+        if n == 0 {
+            break;
+        }
+
+        let n = cmp::min(len - bytes_written, n as u64) as usize;
+        data.extend_from_slice(&buffer[..n]);
+
+        bytes_written += n as u64;
+    }
+
+    Ok(data)
+}
+
+async fn copy_data<W: tokio::io::AsyncWrite + Unpin>(
+    writer: &mut W,
+    reader: &mut File,
+) -> io::Result<()> {
+    let mut buffer = [0; BUFFER_SIZE];
+
+    loop {
+        let n = reader.read(&mut buffer[..]).await?;
+
+        if n == 0 {
+            break;
+        }
+
+        writer.write_all(&buffer).await?;
+    }
+
+    Ok(())
 }
--- lnx-segments/src/lib.rs
+++ lnx-segments/src/lib.rs
@@ -1,9 +1,15 @@
-mod blocking;
 mod aio;
+mod blocking;
 
+mod meta_merger;
 mod metadata;
 
-pub use metadata::{Metadata, get_metadata_offsets, METADATA_HEADER_SIZE};
+pub use metadata::{get_metadata_offsets, Metadata, METADATA_HEADER_SIZE};
 
 pub type Exporter = blocking::exporter::BlockingExporter;
 
+pub static IGNORED_PREFIX: &str = ".tmp";
+pub static IGNORED_FILES: &[&str] = &[".tantivy-meta.lock", ".tantivy-write.lock"];
+pub static META_FILE: &str = "meta.json";
+pub static MANAGED_FILE: &str = ".managed.json";
+pub static SPECIAL_FILES: &[&str] = &[META_FILE, MANAGED_FILE];
