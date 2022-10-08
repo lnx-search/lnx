3b04fe78eded8037b8f57c143047df119d9e76a2
ChillFish8 <hburt2003@gmail.com>
2022-10-08 13:19:18 +0100

    Add combiner to runtime

--- lnx-segments/src/aio/runtime.rs
+++ lnx-segments/src/aio/runtime.rs
@@ -1,10 +1,12 @@
+use std::io;
 use std::time::Duration;
 use glommio::{LocalExecutorPoolBuilder, PoolPlacement, PoolThreadHandles};
 use once_cell::sync::OnceCell;
+use crate::aio::combiner::{AioCombinerActor, AioCombinerActorSetup};
 
 static RUNTIME: OnceCell<AioRuntime> = OnceCell::new();
 
-pub(crate) async fn spawn_actor(task: AioTaskActor) -> Result<(), DeadRuntime>{
+pub(super) async fn spawn_actor(task: AioTask) -> Result<(), DeadRuntime>{
     let rt = RUNTIME.get().ok_or(DeadRuntime)?;
     rt.tasks_tx.send_async(task).await.map_err(|_| DeadRuntime)
 }
@@ -35,23 +37,31 @@ pub fn try_init(num_threads: usize) -> glommio::Result<(), ()> {
 }
 
 
-pub(crate) enum AioTaskActor {
-
+pub(super) enum AioTask {
+    Combiner(AioCombinerActorSetup),
 }
 
-impl AioTaskActor {
-    pub async fn run_actor(mut self) {
+impl From<AioCombinerActorSetup> for AioTask {
+    fn from(v: AioCombinerActorSetup) -> Self {
+        Self::Combiner(v)
+    }
+}
 
+impl AioTask {
+    async fn run_actor(self) -> io::Result<()> {
+        match self {
+            Self::Combiner(inner) => inner.run_actor().await,
+        }
     }
 }
 
 
-pub struct AioRuntime {
+struct AioRuntime {
     pool: PoolThreadHandles<()>,
-    tasks_tx: flume::Sender<AioTaskActor>,
+    tasks_tx: flume::Sender<AioTask>,
 }
 
-async fn run_tasks(tasks: flume::Receiver<AioTaskActor>) {
+async fn run_tasks(tasks: flume::Receiver<AioTask>) {
     while let Ok(task) = tasks.recv_async().await {
         glommio::spawn_local(task.run_actor()).detach();
     }
