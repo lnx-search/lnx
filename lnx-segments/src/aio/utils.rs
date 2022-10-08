a2341964b1f2c0268571240b6a4b25a2f8ae69a3
ChillFish8 <hburt2003@gmail.com>
2022-10-08 16:59:05 +0100

    Add unit tests

--- lnx-segments/src/aio/utils.rs
+++ lnx-segments/src/aio/utils.rs
@@ -5,6 +5,7 @@ use glommio::io::{DmaStreamReader, ImmutableFile};
 
 
 use crate::{get_metadata_offsets, Metadata, METADATA_HEADER_SIZE};
+use crate::aio::AioWriter;
 
 pub(crate) async fn read_metadata(
     file: &ImmutableFile,
@@ -115,6 +116,34 @@ pub(super) async fn read_n_bytes(
     Ok(data)
 }
 
+pub(super) async fn copy_data(
+    writer: &mut AioWriter,
+    reader: &mut DmaStreamReader,
+    buffer: &mut [u8],
+    buffer_offset: &mut usize,
+    expected_len: usize,
+) -> io::Result<()> {
+    let mut read_bytes = *buffer_offset;
+    let mut remaining_len = expected_len;
+
+    while read_bytes < expected_len {
+        let n = reader.read(&mut buffer[*buffer_offset..]).await?;
+
+        if n == 0 {
+            break;
+        }
+
+        let slice_at = cmp::min(n, remaining_len);
+        writer.write_all(&buffer[..slice_at]).await?;
+
+        retain_excess(n, remaining_len, buffer, buffer_offset);
+
+        read_bytes += n;
+        remaining_len -= slice_at;
+    }
+
+    Ok(())
+}
 
 fn retain_excess(
     n_read: usize,
@@ -132,5 +161,146 @@ fn retain_excess(
         buffer.copy_within(retain_byte_from.., 0);
     } else {
         (*buffer_offset) = 0;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::env::temp_dir;
+    use std::path::Path;
+    use datacake_crdt::{get_unix_timestamp_ms, HLCTimestamp};
+    use glommio::ByteSliceMutExt;
+    use glommio::io::{DmaFile, DmaStreamReaderBuilder};
+    use crate::aio::BUFFER_SIZE;
+    use super::*;
+
+    static BUFFER_SAMPLE: &[u8] = b"Hello, world!";
+
+    macro_rules! run {
+        ($fut:expr, $name:expr) => {{
+            glommio::LocalExecutorBuilder::default()
+                .name($name)
+                .spawn($fut)
+                .expect("failed to spawn local executor")
+                .join()
+                .expect("join runtime.")
+        }};
+    }
+
+    async fn get_temp_file(name: &str) -> io::Result<DmaFile> {
+        let fp = temp_dir().join(name);
+
+        let file = glommio::io::OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .dma_open(fp)
+            .await?;
+
+        let mut buffer = file.alloc_dma_buffer(512);
+        buffer.write_at(0, BUFFER_SAMPLE);
+
+        file.write_at(buffer, 0).await?;
+        file.fdatasync().await?;
+
+        Ok(file)
+    }
+
+    #[test]
+    fn test_ensure_reader_at_range_start() -> io::Result<()> {
+        let fut = move || async move {
+            const TARGET_LENGTH: u64 = 2;
+
+            let file = get_temp_file("ensure-reader-test.txt").await?;
+            let mut reader = DmaStreamReaderBuilder::new(file).build();
+
+            let mut buffer = Box::new([0; BUFFER_SIZE]);
+            let mut buffer_offset = 0;
+            ensure_reader_at_range_start(
+                &mut reader,
+                &mut buffer[..],
+                    &mut buffer_offset,
+                TARGET_LENGTH,
+            ).await?;
+
+            assert!(reader.current_pos() >= TARGET_LENGTH, "Expected reader position to be at least upto the targeted start.");
+
+            // This is potentially flakey, but there's not a whole lot we can do about it.
+            if reader.current_pos() > TARGET_LENGTH {
+                let diff = (reader.current_pos() - TARGET_LENGTH) as usize;
+                assert_eq!(buffer_offset, diff, "Expected excess read data to be reflected in the offset.");
+            }
+
+            Ok::<_, io::Error>(())
+        };
+
+        run!(fut, "ensure-reader-start")
+    }
+
+    #[test]
+    fn test_read_n_bytes() -> io::Result<()> {
+        let fut = move || async move {
+            let file = get_temp_file("read-n-bytes.txt").await?;
+            let mut reader = DmaStreamReaderBuilder::new(file).build();
+
+            let mut buffer = Box::new([0; BUFFER_SIZE]);
+            let mut buffer_offset = 0;
+
+            let buff = read_n_bytes(
+                &mut reader,
+                &mut buffer[..],
+                &mut buffer_offset,
+                BUFFER_SAMPLE.len(),
+            ).await?;
+
+            assert_eq!(&buff, BUFFER_SAMPLE, "Expected read buffer and sample to match.");
+
+            Ok::<_, io::Error>(())
+        };
+
+        run!(fut, "test-read-bytes")
+    }
+
+    #[test]
+    fn test_copy_data() -> io::Result<()> {
+        let fut = move || async move {
+            let fp = temp_dir().join("copy-data-writer");
+            let segment_id = HLCTimestamp::new(get_unix_timestamp_ms(), 0, 0);
+
+            println!("gfoo");
+            let mut writer = AioWriter::create(
+                &fp,
+                0,
+                "test-index".to_string(),
+                segment_id
+            ).await?;
+            println!("gfoo");
+
+            let file = get_temp_file("copy-data.txt").await?;
+            println!("ki");
+            let mut reader = DmaStreamReaderBuilder::new(file).build();
+
+            let mut buffer = Box::new([0; BUFFER_SIZE]);
+            let mut buffer_offset = 0;
+
+            copy_data(
+                &mut writer,
+                &mut reader,
+                &mut buffer[..],
+                &mut buffer_offset,
+                BUFFER_SAMPLE.len(),
+            ).await?;
+
+            assert_eq!(
+                writer.current_pos(),
+                BUFFER_SAMPLE.len() as u64,
+                "Expected writer to have written {} bytes.",
+                BUFFER_SAMPLE.len(),
+            );
+
+            Ok::<_, io::Error>(())
+        };
+
+        run!(fut, "test-copy-data")
     }
 }
