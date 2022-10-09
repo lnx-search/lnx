619c441ae1ecaecc6ad61f9f8981ad4e1cea5139
ChillFish8 <hburt2003@gmail.com>
2022-10-09 12:46:02 +0100

    Move read_range into utils file

--- lnx-segments/src/blocking/utils.rs
+++ lnx-segments/src/blocking/utils.rs
@@ -1,4 +1,6 @@
 use std::io::{ErrorKind, SeekFrom};
+use std::ops::Range;
+use std::cmp;
 
 use tokio::fs::File;
 use tokio::io;
@@ -33,4 +35,33 @@ pub(crate) async fn read_metadata(file: &mut File) -> io::Result<Metadata> {
     let metadata = Metadata::from_bytes(&buffer[..len as usize])?;
 
     Ok(metadata)
+}
+
+/// Reads a set of bytes from the given file and makes sure the cursor is correctly
+/// repositioned if more than the required amount of bytes are read.
+///
+/// NOTE:
+///  The start of the `range` is ignored.
+pub(crate) async fn read_range(reader: &mut File, range: Range<u64>) -> io::Result<Vec<u8>> {
+    reader.seek(SeekFrom::Start(range.start)).await?;
+
+    let mut data = vec![];
+
+    let len = range.end - range.start;
+    let mut bytes_written = 0;
+    let mut buffer = crate::new_buffer();
+    while bytes_written < len {
+        let n = reader.read(&mut buffer[..]).await?;
+
+        if n == 0 {
+            break;
+        }
+
+        let n = cmp::min(len - bytes_written, n as u64) as usize;
+        data.extend_from_slice(&buffer[..n]);
+
+        bytes_written += n as u64;
+    }
+
+    Ok(data)
 }
