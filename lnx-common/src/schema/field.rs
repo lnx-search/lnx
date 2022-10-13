56e6bc2745072e81251154fdcbbf3d8feaa870ff
ChillFish8 <hburt2003@gmail.com>
2022-10-13 23:15:40 +0200

    Add documentation and validation of schema

--- lnx-common/src/schema/field.rs
+++ lnx-common/src/schema/field.rs
@@ -32,7 +32,7 @@ pub enum Field {
     /// A document can be associated to any number of facets.
     /// The hierarchy implicitly implies that a document belonging to a facet also belongs to the ancestor of its facet.
     /// In the example above, `/electronics/tv_and_video/` and `/electronics`.
-    Facet(FacetFieldOptions),
+    Facet(BaseOptions),
 
     /// A unsigned 64 bit integer field.
     U64(NumericFieldOptions),
@@ -74,6 +74,50 @@ pub enum Field {
     /// If returned or uploaded as a JSON object this field expects data to be submitted
     /// in the form of a `base64` encoded string and will be returned as such.
     Bytes(NumericFieldOptions),
+}
+
+impl Field {
+    /// Is the field required or not.
+    pub fn is_required(&self) -> bool {
+        match self {
+            Field::Text(opts) => opts.base.required,
+            Field::RawStr(opts) => opts.required,
+            Field::Facet(opts) => opts.required,
+            Field::U64(opts) => opts.base.required,
+            Field::I64(opts) => opts.base.required,
+            Field::F64(opts) => opts.base.required,
+            Field::Json(opts) => opts.required,
+            Field::Bytes(opts) => opts.base.required,
+        }
+    }
+
+    /// Can the field support multiple values or just a single value.
+    pub fn is_multi(&self) -> bool {
+        match self {
+            Field::Text(opts) => opts.base.multi,
+            Field::RawStr(opts) => opts.multi,
+            Field::Facet(opts) => opts.multi,
+            Field::U64(opts) => opts.base.multi,
+            Field::I64(opts) => opts.base.multi,
+            Field::F64(opts) => opts.base.multi,
+            Field::Json(opts) => opts.multi,
+            Field::Bytes(opts) => opts.base.multi,
+        }
+    }
+
+    /// Is the field stored or not.
+    pub fn is_stored(&self) -> bool {        
+        match self {
+            Field::Text(opts) => opts.base.stored,
+            Field::RawStr(opts) => opts.stored,
+            Field::Facet(opts) => opts.stored,
+            Field::U64(opts) => opts.base.stored,
+            Field::I64(opts) => opts.base.stored,
+            Field::F64(opts) => opts.base.stored,
+            Field::Json(opts) => opts.stored,
+            Field::Bytes(opts) => opts.base.stored,
+        }
+    }
 }
 
 #[derive(
@@ -192,26 +236,6 @@ struct NumericFieldOptionsSchema {
 
     #[serde(flatten, default)]
     base: BaseOptions,
-}
-
-#[derive(
-    Debug,
-    Clone,
-    serde::Serialize,
-    serde::Deserialize,
-    rkyv::Archive,
-    rkyv::Serialize,
-    rkyv::Deserialize,
-    utoipa::ToSchema,
-)]
-pub struct FacetFieldOptions {
-    #[schema(default = "false", example = json!(false))]
-    #[serde(default)]
-    /// Should the field be included in the stored and compressed document.
-    ///
-    /// If this is `false`, the  field will not be returned as part of the
-    /// original document, but can still be used for searching.
-    pub stored: bool,
 }
 
 // This is a hack to default to true for serde.
--- lnx-common/src/schema/mod.rs
+++ lnx-common/src/schema/mod.rs
@@ -1,11 +1,10 @@
 mod field;
 mod writer;
 
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, ops::Deref};
 
 pub use field::{
     BaseOptions,
-    FacetFieldOptions,
     Field,
     NumericFieldOptions,
     TextOptions,
@@ -26,7 +25,7 @@ pub use writer::WriterSettings;
 )]
 #[validate(schema(
     function = "validators::validate_schema",
-    skip_on_field_errors = false
+    skip_on_field_errors = true
 ))]
 /// A index schema.
 ///
@@ -34,16 +33,18 @@ pub use writer::WriterSettings;
 /// and manage the index itself, including tokenizers, fields, and validation.
 pub struct Schema {
     #[schema(example = "my-index")]
-    #[validate(length(min = 2, max = 32), custom = "validators::alpha_numeric")]
+    #[validate(length(min = 2, max = 32), custom = "validators::validate_index_name")]
     /// The name of the index.
     ///
     /// This can be made up of alpha-numeric characters, `-` and `_`.
     pub name: String,
 
+    #[serde(default)]
     /// Options and configuration for indexing documents.
     pub indexing_options: WriterSettings,
 
     #[serde(default)]
+    #[validate(custom = "validators::validate_schema_fields")]
     /// The schema's fields.
     ///
     /// If this is set to `null` the system will become schema-less.
@@ -70,22 +71,124 @@ pub struct Schema {
     pub boosted_fields: BTreeMap<String, Score>,
 }
 
+
 mod validators {
-    use std::borrow::Cow;
+    use std::{borrow::Cow, collections::HashSet};
     use std::collections::BTreeMap;
 
     use serde_json::{json, Value};
     use tantivy::Score;
     use validator::ValidationError;
 
-    use super::Schema;
+    use super::{Schema, Field};
 
     pub fn validate_schema(schema: &Schema) -> Result<(), ValidationError> {
+        let fields = match &schema.fields {
+            None => return Ok(()),
+            Some(fields) => fields,
+        };
+
+        let mut error = ValidationError::new("bad_schema");
+        error.message = Some(Cow::Borrowed(
+            "Search fields and boost fields must be defined within the `fields` property when not using the schema-less system.",
+        ));
+        
+        let keys = fields
+            .keys()
+            .cloned()
+            .collect::<HashSet<String>>();
+
+        let param_key = Cow::Borrowed("search_fields");
+        let mut is_fail = false;
+        for field_name in schema.search_fields.iter() {
+            if keys.contains(field_name) {
+                continue;
+            }
+
+            is_fail = true;
+            error
+                .params
+                .entry(param_key.clone())
+                .and_modify(|v| {
+                    if let Value::Array(inner) = v {
+                        inner.push(json!(field_name))
+                    }
+                })
+                .or_insert_with(|| json!(vec![field_name.clone()]));
+        }
+
+        let param_key = Cow::Borrowed("boost_fields");
+        for field_name in schema.search_fields.iter() {
+            if keys.contains(field_name) {
+                continue;
+            }
+
+            is_fail = true;
+            error
+                .params
+                .entry(param_key.clone())
+                .and_modify(|v| {
+                    if let Value::Array(inner) = v {
+                        inner.push(json!(field_name))
+                    }
+                })
+                .or_insert_with(|| json!(vec![field_name.clone()]));
+        }
+
+        if is_fail {
+            return Err(error);
+        }
+
         Ok(())
     }
 
-    pub fn alpha_numeric(value: &str) -> Result<(), ValidationError> {
-        let mut err = ValidationError::new("bad_str");
+    pub fn validate_schema_fields(fields: &BTreeMap<String, Field>) -> Result<(), ValidationError> {
+        let mut error = ValidationError::new("bad_field_name");
+
+        if fields.is_empty() {
+            error.message = Some(Cow::Borrowed(
+                "Must contain at least one field or be set to `null`.",
+            ));
+            return Err(error)    
+        }
+
+        error.message = Some(Cow::Borrowed(
+            "The field name must only contain alpha-numeric characters, `-` or `_` and must not start or end with `-` or `_`.",
+        ));
+
+        let param_key = Cow::Borrowed("keys");
+        let mut is_fail = false;
+        for field_name in fields.keys() {
+            if field_name.starts_with('-') 
+                || field_name.starts_with('_')          
+                || field_name.ends_with('-') 
+                || field_name.ends_with('_')  
+                || field_name
+                    .chars()
+                    .any(|c| !(c.is_alphanumeric() || c == '-' || c == '_'))
+            {
+                is_fail = true;      
+                error
+                    .params
+                    .entry(param_key.clone())
+                    .and_modify(|v| {
+                        if let Value::Array(inner) = v {
+                            inner.push(json!(field_name))
+                        }
+                    })
+                    .or_insert_with(|| json!(vec![field_name.clone()]));
+            }
+        }
+
+        if is_fail {
+            return Err(error);
+        }
+
+        Ok(())
+    }
+
+    pub fn validate_index_name(value: &str) -> Result<(), ValidationError> {
+        let mut err = ValidationError::new("bad_index_name");
         err.params.insert(Cow::Borrowed("value"), json!(value));
 
         if value
@@ -115,7 +218,7 @@ mod validators {
     pub fn validate_boost_fields(
         map: &BTreeMap<String, Score>,
     ) -> Result<(), ValidationError> {
-        let mut error = ValidationError::new("bad_str");
+        let mut error = ValidationError::new("bad_boost_factor");
         error.message = Some(Cow::Borrowed(
             "The boosting multiplier must be greater than `0.0`.",
         ));
@@ -129,10 +232,10 @@ mod validators {
                     .entry(param_key.clone())
                     .and_modify(|v| {
                         if let Value::Array(inner) = v {
-                            inner.push(field.clone().into())
+                            inner.push(json!(field))
                         }
                     })
-                    .or_insert_with(|| Value::Array(vec![field.clone().into()]));
+                    .or_insert_with(|| json!(vec![field.clone()]));
             }
         }
 
@@ -142,7 +245,7 @@ mod validators {
 
 #[cfg(test)]
 mod tests {
-    use field::{BaseOptions, FacetFieldOptions, NumericFieldOptions};
+    use field::{BaseOptions, NumericFieldOptions};
     use utoipa::OpenApi;
 
     use super::*;
@@ -151,7 +254,7 @@ mod tests {
     fn test_utopia() {
         #[derive(OpenApi)]
         #[openapi(
-            components(schemas(Schema, WriterSettings, Field, BaseOptions, NumericFieldOptions, FacetFieldOptions)),
+            components(schemas(Schema, WriterSettings, Field, BaseOptions, NumericFieldOptions)),
             tags(
                 (name = "todo", description = "Todo items management API")
             )
