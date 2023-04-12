2417c612916af2a8ef77755062610a4378f682a8
ChillFish8 <hburt2003@gmail.com>
2023-04-12 20:54:48 +0100

    Fix build error

--- lnx-storage/src/fragments/mod.rs
+++ lnx-storage/src/fragments/mod.rs
@@ -281,11 +281,12 @@ impl IndexFragmentsWriters {
     ///
     /// This is a no-op on windows.
     async fn sync_directory(&self) -> io::Result<()> {
+        let path = self.env.root_path.clone();
         #[cfg(unix)]
         lnx_executor::spawn_task(async move {
             std::fs::OpenOptions::new()
                 .read(true)
-                .open(&crate::resolvers::fragments_folder())?
+                .open(&crate::resolvers::fragments_folder(&path))?
                 .sync_data()?;
             Ok::<_, io::Error>(())
         })
