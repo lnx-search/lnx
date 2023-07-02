4c162aca53c4b04f789c9c9e51afa3b4d1a93bfc
ChillFish8 <hburt2003@gmail.com>
2023-07-02 12:54:40 +0100

    Add recover template

--- lnx-block-store/src/lib.rs
+++ lnx-block-store/src/lib.rs
@@ -1,6 +1,8 @@
 mod metastore_wrapper;
 mod reader;
 mod writers;
+mod recover;
 
 pub use reader::BlockStoreReader;
 pub use writers::BlockStoreWriter;
+pub use recover::recover_segment;
---
+++ lnx-block-store/src/recover.rs
@@ -0,0 +1,6 @@
+use std::path::Path;
+use futures::io;
+
+pub fn recover_segment(path: &Path) -> io::Result<()> {
+    todo!()
+}
