5bad248397079b9f5f855d431c4d707fa42c035b
ChillFish8 <hburt2003@gmail.com>
2024-07-17 22:50:32 +0100

    Add base server runner

--- lnx-rest/src/lib.rs
+++ lnx-rest/src/lib.rs
@@ -3,7 +3,10 @@ mod models;
 
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 
-use poem_openapi::Tags;
+use anyhow::Context;
+use poem::listener::TcpListener;
+use poem::{Route, Server};
+use poem_openapi::{OpenApiService, Tags};
 
 /// Settings for the REST HTTP server.
 pub struct RestSettings {
@@ -24,6 +27,19 @@ impl Default for RestSettings {
 /// This exposes the provided engine via a REST api with documentation
 /// generated for the OpenAPI spec.
 pub async fn run_rest_server(settings: RestSettings) -> anyhow::Result<()> {
+    let merged_services = index::IndexApiRoutes {};
+
+    let api_service =
+        OpenApiService::new(merged_services, "lnx REST API", env!("CARGO_PKG_VERSION"));
+
+    let ui = api_service.redoc();
+    let app = Route::new().nest("/", api_service).nest("/docs", ui);
+
+    Server::new(TcpListener::bind(settings.listen_address))
+        .run(app)
+        .await
+        .context("Run REST server")?;
+
     Ok(())
 }
 
