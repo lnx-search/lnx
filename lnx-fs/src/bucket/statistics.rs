3e069c82c047160f331c4c5be8f9cd16104c0be0
chillfish8 <hburt2003@gmail.com>
2024-12-22 22:50:27 +0000

    Add type-system toggleable statistics

--- lnx-fs/src/bucket/statistics.rs
+++ lnx-fs/src/bucket/statistics.rs
@@ -1,7 +1,8 @@
-use std::fmt::Debug;
+use std::fmt::{Debug, Formatter};
+use std::ops::{Deref, DerefMut};
 use std::time::Duration;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 /// Statistics relating to the read of a file blob.
 pub struct ReadStatistics {
     /// The number of hits on the file cache.
@@ -18,7 +19,7 @@ pub struct ReadStatistics {
     pub schedule_time: Duration,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 /// Statistics relating to the writing of a file blob.
 pub struct WriteStatistics {
     /// The total amount of bytes written.
@@ -26,3 +27,75 @@ pub struct WriteStatistics {
     /// How many entries from the file cache were removed.
     pub cache_evictions: usize,
 }
+
+#[derive(Debug, Copy, Clone)]
+/// Statistics returning is enabled.
+pub struct On;
+#[derive(Debug, Copy, Clone)]
+/// Statistics returning is disabled.
+pub struct Off;
+
+/// A trait to indicate if statistics are enabled or
+/// disabled via the type system and provides a type wrapper
+/// for transparently returning statistics and the result
+/// or just the result.
+pub trait StatisticsEnabled {
+    type Wrapped<T, S>;
+    
+    /// Wrap the inner `T` with the stats `S` depending on the impl rules.
+    fn wrap<T, S>(inner: T, stats: S) -> Self::Wrapped<T, S>;
+}
+
+impl StatisticsEnabled for On {
+    type Wrapped<T, S> = WithStats<T, S>;
+
+    #[inline]
+    fn wrap<T, S>(inner: T, stats: S) -> Self::Wrapped<T, S> {
+        WithStats {
+            inner,
+            stats,
+        }
+    }
+}
+
+impl StatisticsEnabled for Off {
+    type Wrapped<T, S> = T;
+
+    #[inline]
+    fn wrap<T, S>(inner: T, _stats: S) -> Self::Wrapped<T, S> {
+        inner
+    }
+}
+
+pub struct WithStats<T, S> {
+    inner: T, 
+    stats: S,
+}
+
+impl<T, S> Deref for WithStats<T, S> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
+impl<T, S> DerefMut for WithStats<T, S> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.inner
+    }
+}
+
+impl<T, S> Debug for WithStats<T, S> 
+where 
+    T: Debug,
+    S: Debug,
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("WithStats")
+            .field("inner", &self.inner)
+            .field("stats", &self.stats)
+            .finish()
+    }
+}
+    
