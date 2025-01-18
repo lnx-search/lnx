6675864e34bb6e9ce6d5367d2600d45a86a1c204
chillfish8 <hburt2003@gmail.com>
2025-01-18 19:18:02 +0000

    LNX-173: Add tests to table name validator

--- lnx-query/src/validate/table.rs
+++ lnx-query/src/validate/table.rs
@@ -18,6 +18,7 @@ pub fn validate_and_get_table<'a>(
 
     let help = tables
         .keys()
+        .filter(|name| crate::utils::damerau_levenshtein(table_name, name) < 3)
         .min_by_key(|name| crate::utils::damerau_levenshtein(table_name, name))
         .map(|closest| format!("did you mean {closest:?}?"));
 
@@ -30,4 +31,43 @@ pub fn validate_and_get_table<'a>(
 
     let error = context.build_error(ErrorCode::UnknownTable, message, help);
     Err(error)
+}
+
+#[cfg(test)]
+mod tests {
+    use tantivy::schema::STORED;
+
+    use super::*;
+
+    #[test]
+    fn test_validate_and_get_table_rejects_unknown_table() {
+        let mut schema_builder = tantivy::schema::Schema::builder();
+        schema_builder.add_text_field("example", STORED);
+        let schema = schema_builder.build();
+
+        let mut tables = BTreeMap::new();
+        tables.insert("example".to_string(), LnxTable::new(schema));
+
+        let mut context = ValidatorContext::default();
+        let error = validate_and_get_table(&mut context, &tables, "exampl")
+            .expect_err("Validator should reject unknown tables");
+        assert_eq!(error.code, ErrorCode::UnknownTable);
+        assert_eq!(error.help.as_deref(), Some("did you mean \"example\"?"),);
+        assert_eq!(error.message, "the table \"exampl\" does not exist");
+        assert_eq!(error.location, "");
+    }
+
+    #[test]
+    fn test_validate_and_get_table_accepts_known_table() {
+        let mut schema_builder = tantivy::schema::Schema::builder();
+        schema_builder.add_text_field("example", STORED);
+        let schema = schema_builder.build();
+
+        let mut tables = BTreeMap::new();
+        tables.insert("example".to_string(), LnxTable::new(schema));
+
+        let mut context = ValidatorContext::default();
+        validate_and_get_table(&mut context, &tables, "example")
+            .expect("Validator should allow known table");
+    }
 }
