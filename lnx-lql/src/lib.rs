a318e2cec4252ce071578e3d7846a7afba40b9d3
chillfish8 <hburt2003@gmail.com>
2025-04-04 14:34:36 +0100

    LNX-NONE: First stab at lql

---
+++ lnx-lql/src/lib.rs
@@ -0,0 +1,35 @@
+use pest_derive::Parser;
+
+#[derive(Debug, thiserror::Error)]
+#[error("cannot parse query")]
+pub struct ParseQueryError;
+
+#[derive(Parser)]
+#[grammar = "grammar/lql.pest"]
+struct LqlParser;
+
+
+/// Parse a LQL query into the core AST.
+pub fn parse_query_string(query: &str) -> Result<(), ParseQueryError> {
+
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use pest::Parser;
+    use super::*;
+    
+    #[test]
+    fn test_query_parse()  {
+        let q = include_str!("../tests/queries/full_text_search.lql");
+        match LqlParser::parse(Rule::pipeline, q) {
+            Err(e) => {
+                println!("{e}");
+                return;
+            },
+            Ok(p) => { dbg!(p); }
+        }
+    }
+}
